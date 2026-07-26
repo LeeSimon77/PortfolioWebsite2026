58d87ae1457bd7da3bc582517133daacb319a4cf
Csimon <ceilidh.simon@gmail.com>
2026-07-25 22:53:19 -0500

    Basic Website outline and routing

---
+++ PortfolioWebsite2026/src/Components/ExperienceComponent.jsx
@@ -0,0 +1,11 @@
+function experienceComponent({name, company,role, description, startDate, endDate}) {
+    return (
+        <div>
+            <h1>name</h1>
+            <h2>company</h2>
+            <h3>role</h3>
+            <p>description</p>
+            <p>startDate - endDate</p>
+        </div>
+    );
+}
---
+++ PortfolioWebsite2026/src/Components/ExperiencePage.jsx
@@ -0,0 +1,9 @@
+function ExperiencePage() {
+    let experiences = [];
+
+  return (
+    <div>
+        <h1>Experience</h1>
+    </div>
+  );
+}
