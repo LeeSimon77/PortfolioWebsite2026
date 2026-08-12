2e007183ff85cf6600b194263a2f8b2b75549cf3
Csimon <ceilidh.simon@gmail.com>
2026-08-11 23:16:50 -0500

    removing unused styles

--- PortfolioWebsite2026/src/App.jsx
+++ PortfolioWebsite2026/src/App.jsx
@@ -1,4 +1,3 @@
-import './Styles/App.css'
 import {Routes, Route} from "react-router";
 import WorkExperiencePage from "./Pages/WorkExperiencePage.jsx";
 import VolunteerExperiencePage from "./Pages/VolunteerExperiencePage.jsx";
